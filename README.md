4ef1a69c35160ca03c131fb4715b2f88e6a28ec2
jugol-kumar <jk23717933@gmail.com>
2023-03-13 14:19:16 +0600

    ready for published this package.

---
+++ README.md
@@ -0,0 +1,13 @@
+
+#Laravel Slug Generator package
+- Author : Jugol Kumar
+- email: Jugolkumar23@gmail.com
+---
+
+
+#1. Install The package Using Composer On Your Project.
+
+-- write here full description about your package. 
+
+
+now i will init git in this package .
---
+++ src/config/JugolCategory.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Slug Seperator
+    |--------------------------------------------------------------------------
+    |
+    | This key is used by the Illuminate encrypter service and should be set
+    | to a random, 32 character string, otherwise these encrypted strings
+    | will not be safe. Please do this before deploying an application!
+    |
+    */
+
+    "separator" => "-",
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | max slug endfix counter
+    |--------------------------------------------------------------------------
+    |
+    | This key is used by the Illuminate encrypter service and should be set
+    | to a random, 32 character string, otherwise these encrypted strings
+    | will not be safe. Please do this before deploying an application!
+    |
+    */
+
+    "max_count" => 100
+
+
+
+
+];
+
+
